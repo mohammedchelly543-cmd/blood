555850b84f6d7a1fa20717ad9a9a2d69184a4486
mohammedchelly543-cmd <mohammedchelly543@gmail.com>
2026-05-09 20:00:42 +0200

    fix admin static

--- bloodconnect/settings.py
+++ bloodconnect/settings.py
@@ -95,8 +95,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ─── Auth ─────────────────────────────────────────────────────────────────────
